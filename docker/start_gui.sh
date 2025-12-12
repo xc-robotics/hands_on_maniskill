c2bd30d272ff6bc86130cbd2f44f94fa7c56a2ac
Xi Chen <407081305@qq.com>
2025-12-12 09:38:08 +0800

    worked start_gui.sh somehow

--- docker/start_gui.sh
+++ docker/start_gui.sh
@@ -1,11 +1,17 @@
-xhost +
+xhost +local:docker
 
 sudo docker run -it --rm \
 --gpus all \
 --shm-size=8g \
--e NVIDIA_DRIVER_CAPABILITIES=compute,utility,graphics \
+--network=host \
+--privileged \
+--device=/dev/dri:/dev/dri \
+-e NVIDIA_DRIVER_CAPABILITIES=all \
 -e DISPLAY=$DISPLAY \
+-e QT_X11_NO_MITSHM=1 \
+-e VK_ICD_FILENAMES=/usr/share/vulkan/icd.d/nvidia_icd.json \
 -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+-v $HOME/.Xauthority:/root/.Xauthority:rw \
 -v ${PWD}:/workspace \
 --name maniskill \
 maniskill:latest \
